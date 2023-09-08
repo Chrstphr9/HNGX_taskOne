26ae483d73558ad3c711b782d883462171b8433f
somto <somto565@gmail.com>
2023-09-08 07:41:53 +0100

    updated time again

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ function updateDateTime() {
     const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
     const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
 
+    // Create a new Date object with the current date and time
     const now = new Date();
-    const utcTimeMilliseconds = now.getTime(); 
-    const dayOfWeek = now.toLocaleDateString(undefined, { weekday: 'long' });
 
-    utcTimeElement.textContent = `UTC Time: ${utcTimeMilliseconds}`;
+    // Calculate the UTC+1 offset in milliseconds (1 hour = 3600000 milliseconds)
+    const utcOffsetMilliseconds = 1 * 60 * 60 * 1000; // UTC+1
+
+    // Apply the UTC+1 offset to the current time
+    const timeInUTCPlus1 = new Date(now.getTime() + utcOffsetMilliseconds);
+
+    // Get the day of the week and time in UTC+1
+    const dayOfWeek = timeInUTCPlus1.toLocaleDateString(undefined, { weekday: 'long' });
+    const hours = timeInUTCPlus1.getUTCHours().toString().padStart(2, '0');
+    const minutes = timeInUTCPlus1.getUTCMinutes().toString().padStart(2, '0');
+    const seconds = timeInUTCPlus1.getUTCSeconds().toString().padStart(2, '0');
+
+    utcTimeElement.textContent = `UTC Time: ${hours}:${minutes}:${seconds}`;
     dayOfWeekElement.textContent = `Day of the Week: ${dayOfWeek}`;
 }
 
